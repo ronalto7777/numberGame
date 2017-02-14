448faaeaf6b2f203ea207bd16246c86da208a5e2
Vincentius Ronalto <ronalto7777@gmail.com>
2017-02-14 22:44:57 +0700

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,1 +1,23 @@
-    
+function insertionSort(arr){ //start function
+  for(i = 1; i< arr.length; i++){ 
+    newValue = arr[i];
+    j = i;
+    while(j > 0 && arr[j - 1] > newValue ){
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = newValue; //swapping value
+  }
+  return arr;
+}
+
+myArray = [3, 2, 1, 5, 6]; //repalce it with the value you want
+console.log("Before sorted : ["+" "+myArray.join(", ")+" ]");
+
+var x = insertionSort(myArray);
+
+console.log("After sorted : [ "+ x.join(", ")+" ]");
+//check that here : https://repl.it/FjOf/0
+//Sorry I am a Javascript noob who want to conquer to algorithms and programming 
+//follow me for more tips and tricks
+//train your array skill everyday, array is trickiest villain in the world
